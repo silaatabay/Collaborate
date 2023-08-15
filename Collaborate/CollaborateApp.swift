d5b41a2c88962598ca743d313734d586f0cf8d06
Sıla Atabay <silaatabay2024@gmail.com>
2023-08-15 19:54:27 +0300

    Initial Commit

---
+++ Collaborate/CollaborateApp.swift
@@ -0,0 +1,17 @@
+//
+//  CollaborateApp.swift
+//  Collaborate
+//
+//  Created by Sıla Atabay on 15.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct CollaborateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
